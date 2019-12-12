26b7820c978942aec406c8e118a59869f2f6d8aa
loganzhou1 <zhou_ybyq@foxmail.com>
2019-12-12 14:23:49 +0800

    121219

---
+++ c#/C#常用工具封装/常用工具类/常用工具类/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyTools;
+
+namespace 常用工具类
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Tools tools = new Tools();
+        public void button1_Click(object sender, EventArgs e)
+        {
+            if (tools.IsNumeric(textBox1.Text))
+            {
+                textBox1.AppendText("是数字！");
+            }
+            else
+            {
+                textBox1.AppendText("非数字！");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text.Equals("timer_start"))
+            {
+                timer1.Start();
+                button2.Text = "timer_stop";
+            }
+            else
+            {
+                timer1.Stop();
+                button2.Text = "timer_start";
+            }
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            button1.PerformClick();
+        }
+    }
+}
---
+++ c#/C#常用工具封装/常用工具类/常用工具类/MyTools.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTools
+{
+    class Tools
+    {
+        /// <summary>
+        /// 判断字符串是否为数字
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>是返回true,不是返回false</returns>
+        public bool IsNumeric(string str)//接收一个string类型的参数,保存到str里
+        {
+            if (str==null||str.Length==0)       //验证这个参数是否为空
+                return false; //是，就返回False
+            ASCIIEncoding ascii = new ASCIIEncoding();//new ASCIIEncoding 的实例
+            byte[] bytestr = ascii.GetBytes(str);//把string类型的参数保存到数组里
+
+            foreach (var item in bytestr)//遍历这个数组里的内容
+            {
+                if (item<48||item>57)//判断是否为数字
+                {
+                    return false; //不是，就返回False
+                }
+            }
+            return true; //是，就返回True
+            //备注 数字，字母的ASCII码对照表
+            /*
+            0~9数字对应十进制48－57 
+            a~z字母对应的十进制97－122十六进制61－7A 
+            A~Z字母对应的十进制65－90十六进制41－5A
+            */
+
+        }
+
+
+    }
+}
